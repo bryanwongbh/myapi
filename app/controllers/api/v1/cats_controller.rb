a915ff4d6f8196d0787dbe5994c1bdd222a786dc
Bryan Wong <bwbh87@gmail.com>
2016-10-20 03:43:54 +0000

    Serialized data and corrected controller

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -1,2 +1,11 @@
-class CatsController < ApplicationController
+module Api  
+  module V1
+    class CatsController < ApplicationController
+
+      def index
+        render json: Cat.all
+      end
+
+    end
+  end
 end
